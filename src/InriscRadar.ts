d89bdefb2ddad339c5fc1cbaaab6d7f5455c76d4
Thatcher <thatcher@t2studio.nl>
2024-01-18 15:44:49 +0100

    refactored the heavier functions out

--- src/InriscRadar.ts
+++ src/InriscRadar.ts
@@ -1,8 +1,5 @@
 import { html, css, LitElement } from 'lit';
 import { property } from 'lit/decorators.js';
-import scales from './scales';
-import capitalize from './capitalize';
-
 import {
   Chart as ChartJS,
   RadarController,
@@ -13,8 +10,8 @@ import {
   PointElement,
   RadialLinearScale,
   LineElement,
-  ChartData,
 } from 'chart.js';
+import { parseData, getRadarOptions } from './spiderData.js';
 
 ChartJS.register(
   RadarController,
@@ -27,55 +24,6 @@ ChartJS.register(
   Filler
 );
 
-function parseData(radardata: any) {
-  let converted_data: ChartData;
-  let labels = [];
-  let datasets = [];
-
-  const defaultLabels = [
-    'score',
-    'operations',
-    'cashflow',
-    'capital_structure',
-    'liquidity',
-    'size',
-  ];
-
-  for (const [p] of Object.entries(radardata[Object.keys(radardata)[0]])) {
-    labels.push(p);
-  }
-
-  for (const key of Object.keys(radardata)) {
-    // let scalesInUse[key] = scales[key];
-    const values = [];
-
-    let defaultLabelKey: any;
-    for (defaultLabelKey of Object.keys(defaultLabels)) {
-      values.push(radardata[key][defaultLabels[defaultLabelKey]]);
-    }
-
-    datasets.push({
-      label: scales[key].title,
-      pointBackgroundColor: scales[key].pointColor,
-      pointBorderColor: '#fff',
-      backgroundColor: scales[key].backgroundColor,
-      borderColor: scales[key].color,
-      data: values,
-    });
-  }
-  datasets = datasets.reverse();
-
-  converted_data = {
-    labels: defaultLabels.map(function (x) {
-      return x.replace(/_/, ' ');
-    }),
-    datasets: datasets,
-  };
-
-  console.log(converted_data);
-  return converted_data;
-}
-
 export class InriscRadar extends LitElement {
   static styles = css`
     :host {
@@ -85,11 +33,12 @@ export class InriscRadar extends LitElement {
     }
   `;
 
+  // This is the property definition. It expects JSON data
   @property({ type: Object }) spiderData = {};
 
   render() {
     return html`
-      <div style="width: auto; height: auto;"><canvas id="chart"></canvas></div>
+      <div style="position: relative; height:100vh; width:90vw"><canvas id="chart"></canvas></div>
     `;
   }
 
@@ -104,61 +53,13 @@ export class InriscRadar extends LitElement {
     }
 
     const parsedData = parseData(this.spiderData);
+    const graphOptions = getRadarOptions();
 
     // eslint-disable-next-line no-new
     new ChartJS(ctx, {
       type: 'radar',
       data: parsedData,
-      options: {
-        responsive: true,
-
-        plugins: {
-          // 'legend' now within object 'plugins {}'
-          legend: {
-            display: false,
-          },
-        },
-        scales: {
-          r: {
-            min: 0,
-            max: 100,
-            // stepSize: 20,
-            ticks: {
-              // fontSize: 22,
-              count: 6,
-              color: '#95999e',
-              // backdropColor: '#0B243F',
-              backdropPadding: 2,
-              font: {
-                size: 9,
-                family: 'Suisse, sans-serif',
-                weight: 'normal',
-              },
-            },
-            angleLines: {
-              display: false,
-            },
-            grid: {
-              // borderDash: [5, 5],
-              // borderDashOffset: 0.4,
-              // max: 5,
-              // circular: true,
-              lineWidth: 1,
-              color: '#e1e4e8',
-            },
-            pointLabels: {
-              color: '#0B243F',
-              backdropColor: '#fff',
-              font: {
-                family: 'Suisse, sans-serif',
-                weight: 'bold',
-                size: 15,
-              },
-              backdropPadding: 3,
-            },
-          },
-        },
-      },
+      options: graphOptions
     });
   }
 }
---
+++ src/spiderData.ts
@@ -0,0 +1,114 @@
+import { ChartData, ChartOptions } from 'chart.js';
+import scales from './scales';
+import capitalize from './capitalize';
+
+function parseData(radardata: any) {
+  /**
+   * Accepts a JSON object with spider data and returns a ChartData object
+   *
+   **/
+
+  let converted_data: ChartData;
+  let labels = [];
+  let datasets = [];
+
+  const defaultLabels = [
+    'score',
+    'operations',
+    'cashflow',
+    'capital_structure',
+    'liquidity',
+    'size',
+  ];
+
+  for (const [p] of Object.entries(radardata[Object.keys(radardata)[0]])) {
+    labels.push(p);
+  }
+
+  for (const key of Object.keys(radardata)) {
+    // let scalesInUse[key] = scales[key];
+    const values = [];
+
+    let defaultLabelKey: any;
+    for (defaultLabelKey of Object.keys(defaultLabels)) {
+      values.push(radardata[key][defaultLabels[defaultLabelKey]]);
+    }
+
+    datasets.push({
+      label: scales[key].title,
+      pointBackgroundColor: scales[key].pointColor,
+      pointBorderColor: '#fff',
+      backgroundColor: scales[key].backgroundColor,
+      borderColor: scales[key].color,
+      data: values,
+    });
+  }
+  datasets = datasets.reverse();
+
+  converted_data = {
+    labels: defaultLabels.map(function (x) {
+      return capitalize(x.replace(/_/, ' '));
+    }),
+    datasets: datasets,
+  };
+  return converted_data;
+}
+
+function getRadarOptions(): ChartOptions {
+  /**
+   * Returns the options for the radar chart
+   **/
+
+  return {
+    responsive: true,
+
+    plugins: {
+      // 'legend' now within object 'plugins {}'
+      legend: {
+        display: false,
+      },
+    },
+    scales: {
+      r: {
+        min: 0,
+        max: 100,
+        // stepSize: 20,
+        ticks: {
+          // fontSize: 22,
+          count: 6,
+          color: '#95999e',
+          // backdropColor: '#0B243F',
+          backdropPadding: 2,
+          font: {
+            size: 9,
+            family: 'Suisse, sans-serif',
+            weight: 'normal',
+          },
+        },
+        angleLines: {
+          display: false,
+        },
+        grid: {
+          // borderDash: [5, 5],
+          // borderDashOffset: 0.4,
+          // max: 5,
+          // circular: true,
+          lineWidth: 1,
+          color: '#e1e4e8',
+        },
+        pointLabels: {
+          color: '#0B243F',
+          backdropColor: '#fff',
+          font: {
+            family: 'Suisse, sans-serif',
+            weight: 'bold',
+            size: 15,
+          },
+          backdropPadding: 3,
+        },
+      },
+    },
+  };
+}
+
+export { parseData, getRadarOptions };
