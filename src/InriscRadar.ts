e32a53049d60d381a9e53c2a2c6702b99eaaeee0
Thatcher <thatcher@t2studio.nl>
2024-01-22 18:16:08 +0100

    added labels

--- src/InriscRadar.ts
+++ src/InriscRadar.ts
@@ -13,6 +13,7 @@ import {
 } from 'chart.js';
 import { parseData, getRadarOptions } from './spiderData.js';
 import spiderData from './spider_data.json' assert { type: 'json' };
+import styles from './styles.js';
 
 ChartJS.register(
   RadarController,
@@ -32,30 +33,120 @@ export class InriscRadar extends LitElement {
       height: 100%;
       width: 100%;
     }
-  `;
+    .labels {
+      text-size-adjust: 100%;
+      tab-size: 4;
+      font-feature-settings: normal;
+      font-variation-settings: normal;
+      line-height: inherit;
+      font-size: 14px;
+      font-family: Suisse, sans-serif;
+      color: rgb(11 36 63);
+      -webkit-font-smoothing: antialiased;
+      box-sizing: border-box;
+      border-width: 0px;
+      border-style: solid;
+      border-color: rgb(229, 231, 235);
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 0.5rem;
+    }
+
+    .label {
+      color: rgb(11 36 63 / var(--tw-text-opacity));
+      box-sizing: border-box;
+      border-style: solid;
+      border-radius: 0.25rem;
+      border-width: 1px;
+      padding: 0.5rem;
+      padding-top: 3px;
+      padding-bottom: 3px;
+      font-size: 0.75rem;
+      line-height: 1rem;
+      font-weight: 600;
+      text-transform: uppercase;
+      background-color: rgba(167, 215, 96, 1);
+      border-color: rgba(167, 215, 96, 0);
+      position: relative;
+    }
+
+    .years {
+      -webkit-text-size-adjust: 100%;
+      tab-size: 4;
+      font-feature-settings: normal;
+      font-variation-settings: normal;
+      -webkit-tap-highlight-color: transparent;
+      font-family: Suisse, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      box-sizing: border-box;
+      border-width: 0;
+      border-style: solid;
+      border-color: #e5e7eb;
+      margin: 0;
+      margin-bottom: 1.5rem;
+      margin-top: 2rem;
+      text-align: center;
+      font-size: 0.75rem;
+      line-height: 1rem;
+      text-transform: uppercase;
+      color: rgb(11 36 63 / 0.9);
+      position: relative;
+    }`;
 
   // This is the property definition. It expects stringified JSON data
   @property({
     type: String,
     hasChanged(newVal: string, oldVal: string) {
-      console.log("comparing new ", newVal, "and old", oldVal)
       return newVal === oldVal;
-    }
-  }) data = '';
-  
+    },
+  })
+  data = '';
+
   @property({ type: Boolean }) testMode = false;
 
+  @property({ type: String }) usedYears = '';
+
   render() {
-    const testModeWarning = (this.testMode === true) ? 'Graph test mode is enabled' : '';
-    return html`<div style="position: relative; height: 100%; width: 100%">
-        ${testModeWarning}
-        <canvas id="chart"></canvas>
-      </div>`;
+    const testModeWarning =
+      this.testMode === true ? 'Graph test mode is enabled' : '';
+
+    const labels = Object.keys(JSON.parse(this.data));
+    const legenda: string[] = [];
+    const options = ['realized', 'projected', 'combined'];
+
+    options.forEach((option: string) => {
+      if (labels.includes(option)) {
+        legenda.push(option);
+      }
+    });
+
+    return html`
+      ${testModeWarning}
+      <div style="position: relative; height: 100%; width: auto;">
+        <canvas id="chart" class="chart"></canvas>
+      </div>
+
+      <div class="legend" style="">
+      <div class="labels">
+        ${legenda.map(
+          label =>
+            html`<div
+              class="label"
+              style="background-color: ${styles[label].color}"
+            >
+              ${styles[label].title}
+            </div>`
+        )}
+      </div>
+      <p class="years">${this.usedYears}</p>
+      </div>
+    `;
   }
-  
+
   async renderInriscSpider(data: any) {
     const parsedData = parseData(JSON.parse(data));
-    
+
     const radarCanvas = this.renderRoot.querySelector(
       '#chart'
     ) as HTMLCanvasElement;
@@ -64,29 +155,28 @@ export class InriscRadar extends LitElement {
     if (ctx === null) {
       return;
     }
-    
+
     const graphOptions = getRadarOptions();
 
     // eslint-disable-next-line no-new
     new ChartJS(ctx, {
       type: 'radar',
       data: parsedData,
-      options: graphOptions
+      options: graphOptions,
     });
   }
 
   updated() {
-    if (this.data === "") {
-      return
+    if (this.data === '') {
+      return;
     }
 
-    this.renderInriscSpider(this.data)
+    this.renderInriscSpider(this.data);
   }
 
   firstUpdated() {
     if (this.testMode === true) {
       this.renderInriscSpider(JSON.stringify(spiderData));
     }
-   
   }
 }
---
+++ src/styles.ts
@@ -0,0 +1,39 @@
+
+type Style = {
+  color: string,
+  pointColor: string,
+  backgroundColor: string,
+  borderColor: string,
+  title: string,
+};
+
+type Styles = {
+  [key: string]: Style;
+};
+
+const scales: Styles = {
+  realized: {
+    color: 'rgba(167, 215, 96, 1)',
+    pointColor: 'rgba(167, 215, 96, 1)',
+    backgroundColor: 'rgba(167, 215, 96, 0.1)',
+    borderColor: 'rgba(167, 215, 96, 0)',
+    title: 'Realized financials',
+  },
+  combined: {
+    color: 'rgba(61, 76, 178, 0.3)',
+    pointColor: 'rgba(61, 76, 178, 1)',
+    backgroundColor: 'rgba(61, 76, 178, 0.2)',
+    borderColor: 'rgba(61, 76, 178, 0)',
+    title: 'Combined with projected',
+  },
+  projection: {
+    color: 'rgba(231, 152, 83, 0.6)',
+    pointColor: 'rgba(231, 152, 83, 1)',
+    backgroundColor: 'rgba(231, 152, 83, 0.6)',
+    borderColor: 'rgba(231, 152, 83, 0)',
+    title: 'Projections only',
+  },
+};
+
+export default scales;
+export type { Style, Styles };
